1929b21bde8ade60d364617d36c673948865b41c
Noah Sanchez <alu0101134956@ull.edu.es>
2021-04-28 23:35:51 +0100

    Añadiendo ejer 3

---
+++ src/Ejercicio3.ts
@@ -0,0 +1,45 @@
+const fs = require('fs');
+import * as yargs from 'yargs';
+
+function userWatcher(name: string) {
+  let watching: boolean = false;
+  let path: string = ""
+  if (name.length > 0) {
+    path = `/home/usuario/DSI/Pr08/src/database/${name}.json`
+  } else {
+    path = `/home/usuario/DSI/Pr08/src/database`
+  }
+  fs.watch(path, (eventType: any, filename: any) => {
+    if (watching) return;
+    watching = true;
+    console.log("El tipo de evento es " + eventType);
+    if (filename) {
+      console.log(`Fichero: ${filename}`);
+    } else {
+      console.log('Fichero no encontrado');
+    }
+    setTimeout(() => {
+      watching = false;
+  }, 100);
+  })
+  console.log("Esperando cambios en algun fichero...");
+}
+
+yargs.command({
+  command: 'user',
+  describe: 'Usuario',
+  builder: {
+    name: {
+      describe: 'Nombre del Usuario',
+      demandOption: true,
+      type: 'string',
+    },
+  },
+  handler(argv) {
+    if (typeof argv.name === 'string') {
+      userWatcher(argv.name);
+    }
+  },
+});
+
+yargs.parse();
