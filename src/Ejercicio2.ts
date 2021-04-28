f22176b6b37c27e5d068372f6f2c88db499a2baf
Noah Sanchez <alu0101134956@ull.edu.es>
2021-04-28 17:13:32 +0100

    Añadiendo ejer 2

--- src/Ejercicio2.ts
+++ src/Ejercicio2.ts
@@ -1,7 +1,10 @@
 import {access, constants, watch} from 'fs';
+const readline = require('readline');
+const fs = require('fs');
+import {spawn} from 'child_process'
 import * as yargs from 'yargs';
 
-function fileCounter(filename: string) {
+function fileLineCounter(filename: string) {
   if (filename === undefined) {
     console.log('Please, specify a file');
   } else {
@@ -9,12 +12,99 @@ function fileCounter(filename: string) {
       if (err) {
         console.log(`File ${filename} does not exist`);
       } else {
-        console.log(`Starting to watch file ${filename}`);
-        const watcher = watch(filename);
-        watcher.on('change', () => {
-          console.log(`File ${filename} has been modified somehow`);
-        });
-        console.log(`File ${filename} is no longer watched`);
+        const wc = spawn('wc', ['-l', filename]);
+        let wcOutput = '';
+        wc.stdout.on('data', (piece) => wcOutput += piece);
+        wc.on('close', () => {
+          const wcOutputArray = wcOutput.split(/\s+/);
+          console.log("El fichero " + filename + " tiene " + wcOutputArray[0] + " lineas");
+        })
+      }
+    });
+  }
+}
+
+function fileWordCounter(filename: string) {
+  if (filename === undefined) {
+    console.log('Please, specify a file');
+  } else {
+    access(filename, constants.F_OK, (err) => {
+      if (err) {
+        console.log(`File ${filename} does not exist`);
+      } else {
+        const wc = spawn('wc', ['-w', filename]);
+        let wcOutput = '';
+        wc.stdout.on('data', (piece) => wcOutput += piece);
+        wc.on('close', () => {
+          const wcOutputArray = wcOutput.split(/\s+/);
+          console.log("El fichero " + filename + " tiene " + wcOutputArray[0] + " palabras");
+        })
+      }
+    });
+  }
+}
+
+
+function fileCharCounter(filename: string) {
+  if (filename === undefined) {
+    console.log('Please, specify a file');
+  } else {
+    access(filename, constants.F_OK, (err) => {
+      if (err) {
+        console.log(`File ${filename} does not exist`);
+      } else {
+        const wc = spawn('wc', ['-c', filename]);
+        let wcOutput = '';
+        wc.stdout.on('data', (piece) => wcOutput += piece);
+        wc.on('close', () => {
+          const wcOutputArray = wcOutput.split(/\s+/);
+          console.log("El fichero " + filename + " tiene " + wcOutputArray[0] + " caracteres");
+        })
+      }
+    });
+  }
+}
+
+function pipeLineCounter(filename: string) {
+  if (filename === undefined) {
+    console.log('Please, specify a file');
+  } else {
+    access(filename, constants.F_OK, (err) => {
+      if (err) {
+        console.log(`File ${filename} does not exist`);
+      } else {
+        const wcl = spawn('wc', ['-l', filename]);
+        wcl.stdout.pipe(process.stdout);
+      }
+    });
+  }
+}
+
+function pipeWordCounter(filename: string) {
+  if (filename === undefined) {
+    console.log('Please, specify a file');
+  } else {
+    access(filename, constants.F_OK, (err) => {
+      if (err) {
+        console.log(`File ${filename} does not exist`);
+      } else {
+        const wcw = spawn('wc', ['-w', filename]);
+        wcw.stdout.pipe(process.stdout);
+      }
+    });
+  }
+}
+
+function pipeCharCounter(filename: string) {
+  if (filename === undefined) {
+    console.log('Please, specify a file');
+  } else {
+    access(filename, constants.F_OK, (err) => {
+      if (err) {
+        console.log(`File ${filename} does not exist`);
+      } else {
+        const wcc = spawn('wc', ['-c', filename]);
+        wcc.stdout.pipe(process.stdout);
       }
     });
   }
@@ -32,8 +122,45 @@ yargs.command({
   },
   handler(argv) {
     if (typeof argv.file === 'string') {
-      console.log(argv.file);
-      fileCounter(argv.file);
+      fileLineCounter(argv.file);
+      pipeLineCounter(argv.file);
+    }
+  },
+});
+
+yargs.command({
+  command: 'words',
+  describe: 'Numero de palabras',
+  builder: {
+    file: {
+      describe: 'Archivo',
+      demandOption: true,
+      type: 'string',
+    },
+  },
+  handler(argv) {
+    if (typeof argv.file === 'string') {
+      fileWordCounter(argv.file);
+      pipeWordCounter(argv.file);
+    }
+  },
+});
+
+
+yargs.command({
+  command: 'chars',
+  describe: 'Numero de caracteres',
+  builder: {
+    file: {
+      describe: 'Archivo',
+      demandOption: true,
+      type: 'string',
+    },
+  },
+  handler(argv) {
+    if (typeof argv.file === 'string') {
+      fileCharCounter(argv.file);
+      pipeCharCounter(argv.file);
     }
   },
 });
